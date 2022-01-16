3b75fcb499f6a14bd34ac049868f4c4204dde628
Hamza Mohammad <alsorkhyy@gmail.com>
2022-01-15 21:37:16 -0500

    Minor updates

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,6 @@ void main() async {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
